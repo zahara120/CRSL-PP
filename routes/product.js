962d8d89b1e9ce19d5a3ef331cbb31e8d668996a
zahara120 <zaharaa120@gmail.com>
2024-07-18 12:02:22 +0700

    fixed: middleware for admin

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ const ProductController = require('../controllers/productController');
 const product = require('express').Router()
 
 product.get('/', ProductController.showListProduct)
+product.get('/:id/detail', ProductController.detailProduct)
 
 product.use(function (req, res, next) {
     res.locals.user = req.session.user
@@ -15,7 +16,6 @@ product.use(function (req, res, next) {
 
 product.get('/add', ProductController.addProduct)
 product.post('/add', ProductController.saveProduct)
-product.get('/:id/detail', ProductController.detailProduct)
 product.get('/:id/edit', ProductController.editProduct)
 product.post('/:id/edit', ProductController.updateProduct)
 product.get('/:id/delete', ProductController.deleteProduct)
