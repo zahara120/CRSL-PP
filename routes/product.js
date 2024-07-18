718f576d93db2d384197378a69ef4f644032171f
zahara120 <zaharaa120@gmail.com>
2024-07-18 11:41:04 +0700

    middleware for admin

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,16 @@ const ProductController = require('../controllers/productController');
 const product = require('express').Router()
 
 product.get('/', ProductController.showListProduct)
+
+product.use(function (req, res, next) {
+    res.locals.user = req.session.user
+    if (req.session.user.role === 'admin') {
+        next()
+    } else {
+        res.redirect('/products?error=anda bukan admin')
+    }
+})
+
 product.get('/add', ProductController.addProduct)
 product.post('/add', ProductController.saveProduct)
 product.get('/:id/detail', ProductController.detailProduct)
